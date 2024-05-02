a38975d598c85af839ae8f9d85eec411e6f75b3a
chainrunner_ <75865704+chainrunner8@users.noreply.github.com>
2024-05-02 19:31:13 +0200

    Updated StratConfig class
    Added: two attributes, timeframe and z_window.

--- strategy/configStrategy.py
+++ strategy/configStrategy.py
@@ -1,10 +1,12 @@
 
 
 class StratConfig:
-    def __init__(self, lookback, stat_sig):
-        self.resolution = str()
+
+    def __init__(self, timeframe, lookback, z_window, stat_sig):
+        self.timeframe = timeframe
         self.lookback = lookback
         self.stat_sig = stat_sig
+        self.z_window = z_window
 
         if self.stat_sig == 0.01:  # 1% = 0 ; 5% = 1 ; 10% = 2
             self.stat_sig_index = 0
@@ -14,4 +16,4 @@ class StratConfig:
             self.stat_sig_index = 2
 
 
-config = StratConfig(lookback=200, stat_sig=0.05)
+config = StratConfig(timeframe='1h', lookback=200, z_window=21, stat_sig=0.05)  # configuration example
